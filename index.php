aa5783dee1dec0fea5b65c72a0e8bf2ef6c3f00a
Debaprasad Tapader <dev.openroot@gmail.com>
2024-12-26 16:01:28 +0530

    submit

--- index.php
+++ index.php
@@ -3,18 +3,18 @@
 	$grid = [];
 	for ($i = 0; $i <= 25; $i++) {
 		for ($j = 0; $j <= 25; $j++) {
-			$grid[$i][$j] = $data[($j * 26) + $i];
+			$grid[$i][$j] = trim($data[($j * 26) + $i]);
 		}
 	}
 
 	echo "<pre>";
-	print_r($grid);
-	echo "</pre>";
-	
-	/*foreach ($grid as $row) {
+
+	foreach ($grid as $row) {
 		foreach ($row as $column) {
-			echo $column;
+			echo $column . " ";
 		}
 		echo "<br>";
-	}*/
+	}
+
+	echo "</pre>";
 ?>

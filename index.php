1414e6c3cc6a73739ee00e006df7ab78dc120abf
Debaprasad Tapader <dev.openroot@gmail.com>
2024-12-26 13:39:14 +0530

    submit

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
-	echo "iceKnives32";
+	$data = file("32.txt");
+	$grid = [];
+	for ($i = 0; $i <= 25; $i++) {
+		for ($j = 0; $j <= 25; $j++) {
+			$grid[$i][$j] = $data[$i+$j];
+		}
+	}
+
+	foreach ($grid as $row) {
+		foreach ($row as $column) {
+			echo $column;
+		}
+		echo "<br>";
+	}
 ?>
